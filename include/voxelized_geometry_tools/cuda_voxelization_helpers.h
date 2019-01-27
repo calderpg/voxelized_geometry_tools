5c21ff2d440fe1c7aaba7462828c67a945a9ecba
Calder Phillips-Grafflin <calder.pg@gmail.com>
2019-01-26 21:00:51 -0500

    Removed legacy CUDA pipeline

--- include/voxelized_geometry_tools/cuda_voxelization_helpers.h
+++ include/voxelized_geometry_tools/cuda_voxelization_helpers.h
@@ -11,47 +11,6 @@ namespace pointcloud_voxelization
 {
 namespace cuda_helpers
 {
-/*
-bool IsAvailable();
-
-float* PreparePointCloud(const int32_t num_points, const float* points);
-
-int32_t* PrepareTrackingGrid(const int64_t num_cells);
-
-void RaycastPoints(
-    const float* device_points_ptr, const int32_t num_points,
-    const float* const pointcloud_origin_transform,
-    const float* const inverse_grid_origin_transform,
-    const float inverse_step_size, const float inverse_cell_size,
-    const int32_t num_x_cells, const int32_t num_y_cells,
-    const int32_t num_z_cells, int32_t* const device_tracking_grid_ptr);
-
-float* PrepareFilterGrid(
-    const int64_t num_cells, const void* host_data_ptr);
-
-void FilterTrackingGrids(
-    const int64_t num_cells, const int32_t num_device_tracking_grids,
-    int32_t* const* device_tracking_grid_ptrs,
-    float* const device_filter_grid_ptr, const float percent_seen_free,
-    const int32_t outlier_points_threshold,
-    const int32_t num_cameras_seen_free);
-
-void RetrieveTrackingGrid(
-    const int64_t num_cells, const int32_t* const device_tracking_grid_ptr,
-    void* host_data_ptr);
-
-void RetrieveFilteredGrid(
-    const int64_t num_cells, const float* const device_filter_grid_ptr,
-    void* host_data_ptr);
-
-void CleanupDeviceMemory(
-    const int32_t num_device_pointclouds, float* const* device_pointcloud_ptrs,
-    const int32_t num_device_tracking_grids,
-    int32_t* const* device_tracking_grid_ptrs, float* device_filter_grid_ptr);
-
-void SetCudaDevice(const int32_t device);
-*/
-
 class CudaVoxelizationHelperInterface
 {
 public:
